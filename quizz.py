5d293417fe37619b2ebb4774cf42904be37a06a4
Atanu <malatiexe@gmail.com>
2022-05-11 01:58:59 +0530

    Add files via upload

---
+++ quizz.py
@@ -0,0 +1,7 @@
+class question:
+    def __init__(self, q_question, q_answer):
+        self.text = q_question
+        self.answer = q_answer
+
+new_q = question("sjhvfjds", "False")
+print(new_q.answer)

01d8b8d751859262af93c684aa4ff2621032d6d6
Atanu <malatiexe@gmail.com>
2022-08-12 20:35:23 +0530

    Delete quizz.py

--- quizz.py
+++
@@ -1,7 +0,0 @@
-class question:
-    def __init__(self, q_question, q_answer):
-        self.text = q_question
-        self.answer = q_answer
-
-new_q = question("sjhvfjds", "False")
-print(new_q.answer)
